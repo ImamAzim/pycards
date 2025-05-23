fae2b82fcc1dbd5f49a6363fb039025f38dcae64
Imam Usmani <imam@e.email>
2025-05-23 22:41:07 +0200

    def table methods

--- src/pycards/models/table.py
+++ src/pycards/models/table.py
@@ -1,12 +1,24 @@
 class Table(object):
 
     """Table class to create and load games"""
+    _active_game = None
 
-    def __init__(self):
-        """TODO: to be defined. """
+
+    def new_game(self, path: str):
+        """create an instance of a game, activate it on the table and save it
+
+        :path: to save the game
+        """
+        pass
+
+    def load_game(self, path: str):
+        """make a game that was previousely saved active
+        on the table
+
+        :path: location of game to load
+        """
         pass
 
 
 if __name__ == '__main__':
     pass
-
