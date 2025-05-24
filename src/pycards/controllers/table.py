aa2fb77c8d207d3f6ba50b406d5940cf61649159
Imam Usmani <imam@e.email>
2025-05-24 22:44:35 +0200

    game property

--- src/pycards/controllers/table.py
+++ src/pycards/controllers/table.py
@@ -1,21 +1,37 @@
+from pycards.models import Game
+
+
 class Table(object):
 
     """Table class to create and load games"""
-    _active_game = None
 
+    @property
+    def game(self) -> Game | None:
+        """active game on table"""
+        return self._game
 
-    def new_game(self, path: str):
+    @game.setter
+    def game(self, value: Game | None):
+        self._game = value
+
+    def __init__(self):
+        """TODO: Docstring for __init__.
+
+        """
+        self.game = None
+
+    def new_game(self, name: str):
         """create an instance of a game, activate it on the table and save it
 
-        :path: to save the game
+        :name: instance will be saved with this name
         """
         pass
 
-    def load_game(self, path: str):
+    def load_game(self, name: str):
         """make a game that was previousely saved active
         on the table
 
-        :path: location of game to load
+        :name: name of the game under it was saved
         """
         pass
 
