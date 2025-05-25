3946b6a3e18c299672d066cb43522e8f53683e28
Imam Usmani <imam@e.email>
2025-05-25 21:20:39 +0200

    first methods of Card

--- src/pycards/models/card.py
+++ src/pycards/models/card.py
@@ -2,6 +2,21 @@ class Card(object):
 
     """Card class to handle a single card"""
 
+    @property
+    def state(self) -> int:
+        """orientation of the card (4 values possible)"""
+        return self._state
+
+    @property
+    def recto_img(self) -> str:
+        """path location of img file of recto side"""
+        return self._recto_img
+
+    @property
+    def verso_img(self) -> str:
+        """path location of img file, verso side"""
+        return self._verso_img
+
     def __init__(self):
         """TODO: to be defined. """
         pass
