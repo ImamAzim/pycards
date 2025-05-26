5b3b3b32d7d349980d13354d84bf7caec20fd2f3
Imam Usmani <imam@e.email>
2025-05-26 20:54:12 +0200

    r game property

--- src/pycards/controllers/table.py
+++ src/pycards/controllers/table.py
@@ -6,22 +6,13 @@ class Table(object):
 
     """Table class to create and load games"""
 
-    @property
-    def game(self) -> Game | None:
-        """active game on table"""
-        return self._game
-
-    @game.setter
-    def game(self, value: Game | None):
-        self._game = value
-
     def __init__(self, gui: GUI):
         """TODO: Docstring for __init__.
 
         """
 
         self._gui = gui
-        self.game = None
+        self._game = None
 
     def new_game(self, name: str):
         """create an instance of a game, activate it on the table and save it
