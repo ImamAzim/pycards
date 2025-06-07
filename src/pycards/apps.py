be23c9732e41bb4a26faff56f66f17a7f24d05bd
ImamAzim <imam@e.email>
2025-06-07 12:36:02 +0200

    add app

---
+++ src/pycards/apps.py
@@ -0,0 +1,13 @@
+class PycarApp(object):
+
+    """pycards app that will use mvc model"""
+
+    def __init__(self):
+        """TODO: to be defined. """
+        pass
+
+    def start(self):
+        """start the pycard app
+
+        """
+        pass
--- src/pycards/launchers.py
+++ src/pycards/launchers.py
@@ -1,6 +1,10 @@
+from pycards.apps import PycarApp
+
+
 def run_pycards():
     """run the full app (pycards)
     :returns: TODO
 
     """
-    pass
+    app = PycarApp()
+    app.start()
